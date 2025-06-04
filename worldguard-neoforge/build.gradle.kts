7f68d02e9197c904614960b86f3ae8a1f436ee7c
Serezjjja <40457527+Serezjjja@users.noreply.github.com>
2025-06-05 02:15:35 +0300

    Fix NeoForge build configuration

--- worldguard-neoforge/build.gradle.kts
+++ worldguard-neoforge/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("java-library")
-    id("maven-publish")
+    `java-library`
     id("net.neoforged.gradle.userdev") version "7.0.170"
+    id("buildlogic.platform")
 }
 
 base.archivesName.set("worldguard-neoforge")
@@ -17,13 +17,18 @@ repositories {
 dependencies {
     implementation(project(":worldguard-core"))
     implementation(libs.worldedit.core)
-    implementation("net.neoforged:neoforge:${project.property("neo_version")}")
+    implementation("net.neoforged:neoforge:${project.property("neo_version")}") {
+        // Minecraft already bundles common libraries, use our versions
+        exclude(group = "com.google.code.gson", module = "gson")
+        exclude(group = "com.google.guava", module = "guava")
+        exclude(group = "it.unimi.dsi", module = "fastutil")
+        exclude(group = "org.apache.logging.log4j", module = "log4j-api")
+        exclude(group = "org.apache.logging.log4j", module = "log4j-core")
+    }
 }
 
-sourceSets.main.resources.srcDir("src/generated/resources")
-
-publishing {
-    publications.register<MavenPublication>("maven") {
-        from(components["java"])
+sourceSets {
+    main {
+        resources.srcDir("src/generated/resources")
     }
 }
